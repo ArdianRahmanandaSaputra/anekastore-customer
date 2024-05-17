d9268131191f66c2f17adf67555fcdade8059e48
ArdianRahmanandaSaputra <arahmanandasaputra@gmail.com>
2024-05-17 20:02:14 +0700

    fix(category): Penambahan Fitur Terjual

--- component/CategoryView.js
+++ component/CategoryView.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import category from '../data/category';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Endpoint from '../data/constant';
 
@@ -25,7 +26,7 @@ const CategoryView = () => {
                 <div className='row'>
                 {
                     categories && categories.map((c) => (
-                        <div className="col-md-4 col-lg-2 shadow-sm" key={c.id}>
+                        <Link to={`/category/${c.id}`} className="col-md-4 col-lg-2 shadow-sm link-underline link-underline-opacity-0 text-dark" key={c.id}>
                             <div className='container border rounded m-0 p-0'>
                                 <div className='p-2'>
                                     <img src={`${Endpoint.CATEGORYIMAGE}${c.photo}`} alt='' className='img img-fluid h-100 w-100'/>
@@ -34,7 +35,7 @@ const CategoryView = () => {
                                     {c.name}
                                 </div>
                             </div>
-                        </div>
+                        </Link>
                     ))
                 }
                 </div>
